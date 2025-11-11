0d71161c5580559d82373a496dedcc6bea276b71
Ted <ted123810@gmail.com>
2025-11-11 16:25:24 -0700

    basic pieces moveset descriptions

--- py_chess.py
+++ py_chess.py
@@ -2,7 +2,23 @@
 #design document created
 #do not program anything that has not been fully designed
 
-#i've just made this change and I need to make sure it is saved
+#pieces are stored in a 2d array
+#the draw() function iterates through this array and draws the current board
 
+#white goes first
+#game asks player for move
+#checks if is validmove based on moveset
+#checks if is validmove based on current board state
+    #this includes a piece being on the move target
+    #or a piece being in the way of the move target
 
-#heres a nother change
+#Basic Movesets for pieces
+#pawn: move one space in color direction, except if first move. can capture diagonal. are transformed when getting to end of board
+#Rook: moves in straight lines, can Castle
+#Knight: Moves in L's, easiest of the high value pieces to program
+#bishops: move in diagonals
+#Queen: combo of Rook and Bishop Movement
+#King: Need to constantly check if the king is in peril
+
+#Scoreboard that keeps track of captured piece values
+
