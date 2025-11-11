85d2bc5b9263d96d3ffccb1917e7cd55b868b9a1
Ted <ted123810@gmail.com>
2025-11-11 16:52:08 -0700

    started working on draw() and draw_line() functions

--- py_chess.py
+++ py_chess.py
@@ -22,3 +22,18 @@
 
 #Scoreboard that keeps track of captured piece values
 
+def draw_line(y_val, board):
+    output = "" #starts empty
+    for x_val in range(8):
+        #x_val, y_val
+        #if there is no piece at location, add an X to the Line output String
+        #else, add piece to output string
+    #after the entire loop, Draw the String
+
+def draw(board):
+    #loop through the 2d board and print a piece if there is one at that location
+    for y in range(8):
+        draw_line(y, board) #draw entire y line
+    #entire board is drawn, maybe add white and black labels?
+    
+
