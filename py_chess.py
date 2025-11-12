190131991d59eb9eb5bfce7881555b25c536c729
Ted <ted123810@gmail.com>
2025-11-11 20:58:03 -0700

    working on printing a line, having trouble

--- py_chess.py
+++ py_chess.py
@@ -22,13 +22,28 @@
 
 #Scoreboard that keeps track of captured piece values
 
+def make_board(mode):
+    board = []
+    if mode == 1:
+        #sparsly populated board
+        board = [((3,4),("W","K")),((7,2),("B","p")),((2,2),("W","P")),((8,1),("B","h"))]
+        return board
+    else:
+        return board
+
 def draw_line(y_val, board):
     output = "" #starts empty
     for x_val in range(8):
         #x_val, y_val
-        #if there is no piece at location, add an X to the Line output String
-        #else, add piece to output string
+        #for every x value in the line
+        #if that x,y loc is in the board list, then put the piece in the output string
+            #if there is a piece at the current board location
+            # 
+        else:
+            #if there is no piece at board location
+            output = output + "X" 
     #after the entire loop, Draw the String
+    print(output)
 
 def draw(board):
     #loop through the 2d board and print a piece if there is one at that location
