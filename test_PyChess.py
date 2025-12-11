8e95d104440c3073475f3b9941d6bb773713cd4c
Teeedor <ted123810@gmail.com>
2025-12-11 12:36:07 -0700

    small changes

---
+++ test_PyChess.py
@@ -0,0 +1,64 @@
+import unittest
+import move_sets
+#also import the functions to test
+
+class TestChess(unittest.TestCase):
+    #ROOK
+    def test_rook_movement():
+        pass
+    def test_rook_capture():
+        pass
+    def test_rook_collisions():
+        pass
+    def test_rook_valid_input():
+        pass
+
+    #BISHOP
+    def test_bishop_movement():
+        pass
+    def test_bishop_capture():
+        pass
+    def test_bishop_collisions():
+        pass
+    def test_bishop_valid_input():
+        pass
+
+    #KNIGHT
+    def test_knight_movement():
+        pass
+    def test_knight_capture():
+        pass
+    def test_knight_collisions():
+        pass
+    def test_knight_valid_input():
+        pass
+
+    #QUEEN
+    def test_queen_movement():
+        pass
+    def test_queen_capture():
+        pass
+    def test_queen_collisions():
+        pass
+    def test_queen_valid_input():
+        pass
+
+    #KING
+    def test_king_movement():
+        pass
+    def test_king_capture():
+        pass
+    def test_king_collisions():
+        pass
+    def test_king_valid_input():
+        pass
+
+    #PAWN
+    def test_pawn_movement():
+        pass
+    def test_pawn_capture():
+        pass
+    def test_pawn_collisions():
+        pass
+    def test_pawn_valid_input():
+        pass
